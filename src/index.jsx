d33bedad63a198246a32a8bf051dcda260664553
IlmamFarizan <ilmam.farizan@yahoo.com>
2023-03-11 02:44:18 +0800

    Added contexts and Upcoming movie page (WIP).

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,7 @@ import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <SiteHeader />      {/* New Header  */}
       <Routes>
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
+        <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
