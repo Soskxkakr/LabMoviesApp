426c46fd8ac8b813fde070aa76e8f7f6a1651bc4
Soskxkakr <aldo.taslim@gmail.com>
2023-04-11 21:19:29 +0800

    Manigga

--- src/components/templateMovieListPage/index.jsx
+++ src/components/templateMovieListPage/index.jsx
@@ -1,24 +1,82 @@
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
 import React from "react";
 import Header from "../headerMovieList";
-import Grid from "@mui/material/Grid";
 import MovieList from "../movieList";
 
 const styles = {
-  root: { 
+  root: {
     backgroundColor: "#bfbfbf",
-  }
+  },
+  grid: {
+    flexGrow: 1,
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "100%",
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
 };
 
-function MovieListPageTemplate({ movies, title, action }) {
-  return (
-    <Grid container sx={styles.root}>
-      <Grid item xs={12}>
-        <Header title={title} />
-      </Grid>
-      <Grid item container spacing={5}>
-      <MovieList action={action} movies={movies} />
-      </Grid>
-    </Grid>
-  );
+function MovieListPageTemplate({ movies, title, action, actors }) {
+  const MaNigga = () => {
+    if (movies) {
+      return (
+        <Grid container sx={styles.root}>
+          <Grid item xs={12}>
+            <Header title={title} />
+          </Grid>
+          <Grid item container spacing={5}>
+            <MovieList action={action} movies={movies} />
+          </Grid>
+        </Grid>
+      );
+    } else if (actors) {
+      return (
+        <>
+          <Grid container spacing={4} sx={styles.grid}>
+            <Grid item xs={12}>
+              <Header title={title} />
+            </Grid>
+            {actors.map((actor) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={actor.id}>
+                <Card sx={styles.card}>
+                  {actor.profile_path ? (
+                    <CardMedia
+                      sx={styles.cardMedia}
+                      image={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
+                      title={actor.name}
+                    />
+                  ) : (
+                    <CardMedia
+                      sx={styles.cardMedia}
+                      title="No Image Available"
+                      component="img"
+                      src="https://via.placeholder.com/500x750/FFFFFF/000000?text=No+Image+Available"
+                      alt="No Image Available"
+                    />
+                  )}
+                  <CardContent sx={styles.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {actor.name}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      );
+    } else {
+      return null;
+    }
+  };
+  return <MaNigga />;
 }
 export default MovieListPageTemplate;
--- src/pages/topActorsPage.jsx
+++ src/pages/topActorsPage.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getPopularActors } from "../api/tmdb-api";
+import PageTemplate from "../components/templateMovieListPage";
 
 const TopActorsPage = () => {
   const [actors, setActors] = useState([]);
@@ -10,29 +11,13 @@ const TopActorsPage = () => {
         const response = await getPopularActors();
         setActors(response.results);
       } catch (error) {
-        console.error(error);
+        console.log(error);
       }
     };
     fetchActors();
   }, []);
 
-  return (
-    <div>
-      <h1>Top Actors</h1>
-      <div className="actors-container">
-        {actors.map((actor) => (
-          <div key={actor.id} className="actor-box">
-            <img
-              src={`https://image.tmdb.org/t/p/w300${actor.profile_path}`}
-              alt={`${actor.name} profile`}
-            />
-            <h2>{actor.name}</h2>
-            <p>{actor.known_for_department}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  return <PageTemplate title="Top Actors" actors={actors} />;
 };
 
 export default TopActorsPage;
